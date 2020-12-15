3f97606e889cf3fdfb406fff1e9f147d6f56c695
Arnel Lenteria <arnel.lenteria@gmail.com>
2020-12-16 02:25:20 +0800

    exclude ppp interfaces from ifaces list

--- assets/scripts/admin/index.js
+++ assets/scripts/admin/index.js
@@ -1,6 +1,7 @@
 (function () {
   'use strict';
-  var App = angular.module('Plugins')
+  var App = angular.module("adopisoft")
+  angular.module('Plugins')
   .config(function($stateProvider) {
     $stateProvider
     .state('plugins.pppoe_server', {
@@ -13,4 +14,38 @@
       },
     });
   });
+
+  // exclude ppp client interfaces from iface list
+  App.config(function ($httpProvider) {
+    $httpProvider.interceptors.push("pppFilter");
+  })
+  .factory("pppFilter", function($q, $rootScope){
+    var service = {
+      request: function(d){
+        return d
+      },
+      response: function(d) {
+        var url = ((d||{}).config||{}).url || ""
+        if(!d.data) return d
+
+        if(url.includes('/interfaces/list')){
+          d.data = d.data.filter(function(i){
+            return !(i||"").match(/^ppp/)
+          })
+        }else if(url.includes("/system/stats/network")){
+          d.data = d.data.filter(function(i){
+            return !((i||{}).interface||"").match(/^ppp/)
+          })
+        }
+
+        return d
+      },
+      responseError: function(rejection) {
+        return $q.reject(rejection);
+      }
+    };
+    return service;
+
+  });
+
 })();
