83b6a3a0f4ceba478e5090b74df54cb222d4e3e5
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-08-01 00:57:02 +0800

    4990ab556010c78d509ee8bcc65ac5420a3a9e85

--- router.js
+++ router.js
@@ -1,16 +1,1 @@
-'use strict'
-
-var core = require('plugin-core')
-var { router, middlewares } = core
-var { express, bodyParser, act } = middlewares
-var pppoe_server_ctrl = require('./controllers/pppoe_server_ctrl.js')
-router.get('/pppoe-server/clients', express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.clients)
-router.get('/pppoe-server/settings', express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.settings)
-router.post('/pppoe-server/settings', act, express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.updateSettings)
-router.post('/pppoe-server/clients', act, express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.createClient)
-router.post('/pppoe-server/clients/:index', act, express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.updateClient)
-router.delete('/pppoe-server/clients/:index', act, express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.deleteClient)
-router.get('/pppoe-server/clients/:index/update-bill', express.urlencoded({ extended: true }), bodyParser.json(), core.middlewares.auth, pppoe_server_ctrl.updateBill)
-router.get('/pppoe-server/on-connected', express.urlencoded({ extended: true }), bodyParser.json(), pppoe_server_ctrl.onConnected)
-router.get('/pppoe-server/on-disconnected', express.urlencoded({ extended: true }), bodyParser.json(), pppoe_server_ctrl.onDisconnected)
-module.exports = router
+"use strict";var core=require("plugin-core");var{router,middlewares}=core;var{express,bodyParser,act}=middlewares;var pppoe_server_ctrl=require("./controllers/pppoe_server_ctrl.js");router.get("/pppoe-server/clients",express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.clients);router.get("/pppoe-server/settings",express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.settings);router.post("/pppoe-server/settings",act,express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.updateSettings);router.post("/pppoe-server/clients",act,express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.createClient);router.post("/pppoe-server/clients/:index",act,express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.updateClient);router.delete("/pppoe-server/clients/:index",act,express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.deleteClient);router.get("/pppoe-server/clients/:index/update-bill",express.urlencoded({extended:true}),bodyParser.json(),core.middlewares.auth,pppoe_server_ctrl.updateBill);router.get("/pppoe-server/on-connected",express.urlencoded({extended:true}),bodyParser.json(),pppoe_server_ctrl.onConnected);router.get("/pppoe-server/on-disconnected",express.urlencoded({extended:true}),bodyParser.json(),pppoe_server_ctrl.onDisconnected);module.exports=router;
